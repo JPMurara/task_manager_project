9a7296c0767a31f0dbee7d3c2affbc6252e7f2fc
Bruno Gomes <brunogomes1198@gmail.com>
2023-08-27 12:13:00 +1000

    Index.html and header component setup

---
+++ client/js/components/header.js
@@ -0,0 +1,9 @@
+function renderHeader() {
+    const header = document.getElementById("header-nav")
+
+    const navUl = document.createElement("ul")
+
+    navUl.classList.add("nav-list")
+
+    
+}
