1df5572375f4c87fbaf5443dda24c27258010bba
Bruno Gomes <brunogomes1198@gmail.com>
2023-09-02 19:51:34 +1000

    Working on dashboard and editing feature

--- client/js/components/dashboard.js
+++ client/js/components/dashboard.js
@@ -1,7 +1,46 @@
 import renderHeader from "./header.js";
 
+function renderDashboard() {
+    //Always show the last activity when user click on Dashboard
+    renderLastActivity();
+
+    const page = document.getElementById("page");
+
+    const content = document.createElement("div");
+    content.classList.add("content");
+    content.id = "main_content";
+
+    //ACTIVITIES INPUT TO GET TASKS
+    const openai = document.createElement("div");
+    openai.classList.add("openai");
+    openai.innerHTML = `
+        <form action="" id="formAI">
+            <input type="text" name="message" id="message" placeholder="Enter the kind of activity..">
+            <button type="submit">Send</button>
+        </form>
+        `;
+
+    content.appendChild(openai);
+    page.replaceChildren(content);
+    renderSidebarActivities();
+    getActivity();
+}
+
 function renderActivity(activity_id) {
     const content = document.getElementById("main_content");
+
+    //Clear the old content, to only show the new one
+    // Remove the existing activity_header, if any
+    const existingHeader = document.querySelector(".activity_header");
+    if (existingHeader) {
+        content.removeChild(existingHeader);
+    }
+
+    // Remove the existing task rows, if any
+    const existingRow = document.querySelector(".row");
+    if (existingRow) {
+        content.removeChild(existingRow);
+    }
 
     axios.get("/api/activity/get/" + activity_id).then((res) => {
         console.log(res);
@@ -34,9 +73,12 @@ function renderActivity(activity_id) {
         //Render list for tasks
         res.data.tasks.forEach((task) => {
             const list = document.createElement("li");
-            list.textContent = task.task_name;
-            // <i class="fa-solid fa-pen-to-square iconEdit"></i>
 
+            list.innerHTML = `${
+                task.task_name
+            } <a href="#" onclick='editTask(${JSON.stringify(
+                task
+            )})'><i class="fa-solid fa-pen-to-square iconEdit"></i></a>`;
             // Append the new task to the container
             todoList.appendChild(list);
         });
@@ -65,29 +107,6 @@ function renderActivity(activity_id) {
 
         content.append(row);
     });
-}
-
-function renderDashboard() {
-    const page = document.getElementById("page");
-
-    const content = document.createElement("div");
-    content.classList.add("content");
-    content.id = "main_content";
-
-    //IF NO ACTIVITY RENDER THIS OTHERWISE RENDER THE NAME ACTIVITY
-    const openai = document.createElement("div");
-    openai.classList.add("openai");
-    openai.innerHTML = `
-        <form action="" id="formAI">
-            <input type="text" name="message" id="message" placeholder="Enter the kind of activity..">
-            <button type="submit">Send</button>
-        </form>
-        `;
-
-    content.appendChild(openai);
-    page.replaceChildren(content);
-    renderSidebarActivities();
-    getActivity();
 }
 
 function renderSidebarActivities() {
@@ -120,11 +139,129 @@ function getActivity() {
         const data = {
             activity: formData.get("message"),
         };
+
         //POST ACTIVITY NAME
-        axios.post("/api/activity", data);
+        axios.post("/api/activity", data).then((res) => {
+            console.log(res.data);
+            //Render the new tasks on dashboard after posting it
+            if (res.data && res.data.activity_id) {
+                renderActivity(res.data.activity_id);
+            }
+
+            //Refresh sidebar activities
+            renderSidebarActivities();
+        });
+    });
+}
+
+//IF NO ACTIVITY TELL THE USER TO SHOW A MESSAGE
+function renderLastActivity() {
+    axios
+        .get("/api/activity/getLast")
+        .then((res) => {
+            if (res.data) {
+                renderActivity(res.data.activity_id);
+            } else {
+                // No activity exists. Handle this case accordingly.
+                // For example, show a message to the user indicating no activities are present.
+                const content = document.getElementById("main_content");
+                const message = document.createElement("h3");
+                message.classList.add("no-activity-message");
+                message.textContent = "No activities found. Please create one!";
+                content.appendChild(message);
+            }
+        })
+        .catch((error) => {
+            console.error("Error fetching the last activity:", error);
+            // Handle any other errors that may arise.
+        });
+}
+
+function editTask(task) {
+    console.log(task);
+    // Create elements
+    const dialog = document.createElement("dialog");
+    dialog.style.display = "flex";
+
+    const form = document.createElement("form");
+    form.id = "editTaskForm";
+    form.action = "/api/task/update/" + task.task_id; // or wherever your backend route is
+    form.method = "PUT";
+
+    form.innerHTML = `
+        <label for="edit_task_name">Name:</label>
+        <input type="text" id="edit_task_name" name="task_name" value="${
+            task.task_name || ""
+        }">
+
+        <label for="edit_task_description">Description:</label>
+        <textarea id="edit_task_description" name="task_description">${
+            task.task_description || ""
+        }</textarea>
+
+        <label for="edit_task_status">Status:</label>
+        <select id="edit_task_status" name="task_status">
+            <option value="pending" ${
+                task.tasks_status === "pending" ? "selected" : ""
+            }>Pending</option>
+            <option value="in_progress" ${
+                task.tasks_status === "in_progress" ? "selected" : ""
+            }>In Progress</option>
+            <option value="completed" ${
+                task.tasks_status === "completed" ? "selected" : ""
+            }>Completed</option>
+        </select>
+
+        <label for="edit_task_priority">Priority:</label>
+        <select id="edit_task_priority" name="task_priority">
+            <option value="low" ${
+                task.task_priority === "low" ? "selected" : ""
+            }>Low</option>
+            <option value="medium" ${
+                task.task_priority === "medium" ? "selected" : ""
+            }>Medium</option>
+            <option value="high" ${
+                task.task_priority === "high" ? "selected" : ""
+            }>High</option>
+        </select>
+
+        <label for="edit_assigned_to">Assigned To:</label>
+        <input type="text" id="edit_assigned_to" name="assigned_to" value="${
+            task.assigned_to || ""
+        }">
+
+        <label for="edit_due_date">Due Date:</label>
+        <input type="date" id="edit_due_date" name="due_date" value="${
+            task.due_date || ""
+        }">
+
+        <input type="submit" value="Save">
+    `;
+
+    const closeBtn = document.createElement("button");
+    closeBtn.textContent = "Close";
+
+    // Add classes to elements for styling
+    dialog.classList.add("dialog");
+    closeBtn.classList.add("close-button");
+    form.classList.add("dialog-form");
+
+    // Append form and button to dialog, then dialog to the document
+    dialog.append(form);
+    dialog.append(closeBtn);
+    document.body.appendChild(dialog);
+
+    // Display the dialog when function is called
+    dialog.showModal();
+
+    // Close the dialog box when the button close is clicked
+    closeBtn.addEventListener("click", function () {
+        dialog.close();
+        document.body.removeChild(dialog); // Optional: remove dialog from DOM after closing
     });
 }
 
 window.renderActivity = renderActivity;
+window.editTask = editTask;
 
 export default renderDashboard;
